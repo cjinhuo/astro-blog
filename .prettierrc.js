7336f7e851d087b29fd12527a7f6662732b20154
Shanks <chenjinhuo@bytedance.com>
2022-09-29 17:03:10 +0800

    feat(all): first commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+// 如果该文件没有存在，就会去找 setting 下的 prettier 配置
+module.exports = {
+  singleQuote: true,
+  endOfLine: 'auto',
+  bracketSameLine: true,
+  proseWrap: 'preserve',
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+};
